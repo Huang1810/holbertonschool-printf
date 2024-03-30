a1beccd6a2409ab634f914c2f80b8be78e74f6af
Huang1810 <ahhamed1810@gmail.com>
2024-03-30 22:09:06 +0100

    fixed empty string case

--- print_string.c
+++ print_string.c
@@ -18,7 +18,7 @@ char *str = va_arg(args, char *);
 if (str == NULL)
 str = "(null)";
 if (str[0] == '\0')
-return (0);
+return (-1);
 for (i = 0; str[i] != '\0'; i++)
 count_fun += _putchar(str[i]);
 return (count_fun);
